f77b6fe75e415b9a16b1e2629ad88b82769bbf58
kzkrjfhrbdbd <roquelorena679@gmail.com>
2025-07-10 19:26:01 +0800

    Create Auto-Complete-Dalgona.lua

---
+++ Auto-Complete-Dalgona.lua
@@ -0,0 +1,23 @@
+
+do
+    local Module = ReplicatedStorage.Modules.Games.DalgonaClient
+
+    function CompleteDalgona()
+        if not getgenv().Toggles.DalgonaAuto then return end
+
+        for _, f in ipairs(getreg()) do
+            if typeof(f) == "function" and islclosure(f) then
+                if getfenv(f).script == Module and debug.getinfo(f).nups == 73 then
+                    setupvalue(f, 31, 9e9)
+                    break
+                end
+            end
+        end
+    end
+
+    local Original
+    Original = hookfunction(require(Module), function(...)
+        task.delay(3, CompleteDalgona)
+        return Original(...)
+    end)
+end

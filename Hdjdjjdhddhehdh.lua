3b3af02b7a38fb9f3f8ad5f8eab89d509ff02f6a
kzkrjfhrbdbd <roquelorena679@gmail.com>
2025-07-21 00:29:29 +0800

    Update Hdjdjjdhddhehdh.lua

--- Hdjdjjdhddhehdh.lua
+++ Hdjdjjdhddhehdh.lua
@@ -4993,7 +4993,7 @@ if targetlib ~= "Wind" then
             pcall(function()
                 Library:Unload()
             end)
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/VapeVoidware/VW-Add/main/inkgame.lua", true))()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/kzkrjfhrbdbd/Skidd/refs/heads/main/Hdjdjjdhddhehdh.lua", true))()
         else
             Script.Functions.Alert("[Save Settings]: Press the button again to reset your settings. This cannot be undone!", 5)
             approved = true
